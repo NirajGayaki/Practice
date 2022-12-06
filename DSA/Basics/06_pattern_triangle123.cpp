498dcf6b6638c7b0b6420aa637668a84acb19d3d
Niraj <nirajgayaki@gmail.com>
2022-12-06 13:17:40 +0530

    Number Pattern : Triangle 123

---
+++ DSA/Basics/06_pattern_triangle123.cpp
@@ -0,0 +1,26 @@
+// GFG : Pattern 3 : Printing the triangle of increasing column number
+#include <iostream>
+using namespace std;
+
+// function definition
+void triangle123(int n) {
+    for (int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// Driver Code
+int main(){
+
+    int n = 0;
+    cout << "Enter the Number : ";
+    cin >> n;
+
+    // function calling
+    triangle123(n);
+
+    system("pause > 0");
+}
