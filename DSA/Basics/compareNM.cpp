0df8b9c44da1bbd92650a543463cfbad733f48d6
Niraj <nirajgayaki@gmail.com>
2022-12-06 10:08:53 +0530

    CompareNM.cpp file added

---
+++ DSA/Basics/compareNM.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function Definition
+string compareNM(int n, int m){
+    if(n < m){return "lesser";}
+    else if(n == m){return "equal";}
+    else{return "greater";}
+}
+
+// Driver Code
+int main(){
+    int n = 5;
+    int m = 9;
+
+    // Function Calling
+    string result = compareNM(n, m);
+
+    cout << result;
+
+    system("pause > 0");
+}
